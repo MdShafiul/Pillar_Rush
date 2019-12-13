5cec9098d69e3e3f077bcc3b3ca74eb3185e10a6
MdShafiul <56889215+MdShafiul@users.noreply.github.com>
2019-12-13 18:59:56 +0600

    Modify Prefabs

---
+++ Assets/Script/ScoreRotationWithCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreRotationWithCamera : MonoBehaviour
+{
+    public Camera base_camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + base_camera.transform.rotation * Vector3.forward, base_camera.transform.rotation * Vector3.up);
+    }
+}
