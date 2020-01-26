2f9d08c7fce4a8feb781d022d8ce77166bc3b2f5
MdShafiul <56889215+MdShafiul@users.noreply.github.com>
2020-01-27 01:51:16 +0600

    Update floating_text and update gitignore
    rotate 3d text with camera

---
+++ Assets/Script/UI_with_camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_with_camera : MonoBehaviour
+{
+
+    public Camera base_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        base_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + base_camera.transform.rotation * Vector3.back * (-1.0f), base_camera.transform.rotation * Vector3.up);
+    }
+}
